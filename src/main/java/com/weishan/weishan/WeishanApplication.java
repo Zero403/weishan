7703965ba53a6874ecaa5d5716d791c25e3c9641
kilo <h_x_123@163.com>
2019-05-05 19:03:02 +0800

    修正错误的字段

--- src/main/java/com/weishan/weishan/WeishanApplication.java
+++ src/main/java/com/weishan/weishan/WeishanApplication.java
@@ -1,11 +1,15 @@
 package com.weishan.weishan;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@MapperScan("com.weishan.weishan.dao")
 public class WeishanApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
